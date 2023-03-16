67509ea637881fd3e43c534629264058e9326669
Alex <alexmwangithuku001@gmail.com>
2023-03-16 13:15:31 +0300

    Done

---
+++ app/controllers/plants_controller.rb
@@ -0,0 +1,33 @@
+class PlantsController < ApplicationController
+  def index
+    plants = Plant.all
+    render json: plants
+  end
+
+  def show
+    plant = Plant.find(params[:id])
+    render json: plant
+  end
+
+  def create
+    plant = Plant.new(plant_params)
+
+    if plant.save
+      render json: plant, status: :created
+    else
+      render json: { errors: plant.errors }, status: :unprocessable_entity
+    end
+  end
+
+  def create
+    plant = Plant.new(name: params[:name], image: params[:image], price: params[:price])
+  
+    if plant.save
+      render json: plant, status: :created
+    else
+      render json: plant.errors, status: :unprocessable_entity
+    end
+  end
+  
+
+end
